75b5d59e4013a2223ce804473a47bfd9cc20220b
yu <hsinyu.lo@mail.utoronto.ca>
2021-04-04 20:43:40 -0700

    Add test user info in home page

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import { Text } from 'react-native';
+import { useState, useEffect } from "react";
+import React from "react";
+import { Auth } from "aws-amplify";
+import { Text, SafeAreaView } from "react-native";
 
 const Home = () => {
-    return <Text>Home</Text>
-}
+  const [email, setEmail] = useState("");
 
-export default Home
+  useEffect(() => {
+    Auth.currentUserInfo().then((userInfo) => {
+      const { attributes = {} } = userInfo;
+      setEmail(attributes.email)
+    });
+  }, []);
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+      <Text>{email}</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
