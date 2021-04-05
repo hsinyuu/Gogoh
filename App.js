0ecaf438b0f0ccc10734f74df90395b59adb730f
yu <hsinyu.lo@mail.utoronto.ca>
2021-04-04 20:42:00 -0700

    Add basic authentication and logout

--- App.js
+++ App.js
@@ -2,9 +2,15 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import MainBottomTabs from 'navigations/MainBottomTabs'
+import MainBottomTabs from "navigations/MainBottomTabs";
+import Amplify from "aws-amplify";
+import { withAuthenticator, AmplifyTheme } from "aws-amplify-react-native";
 
-export default function App() {
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
+function App() {
   return (
     <NavigationContainer>
       <MainBottomTabs />
@@ -12,11 +18,9 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const MyInput = Object.assign({}, AmplifyTheme.Input, {borderColor: 'grey', padding: 16, borderWidth: 1, borderRadius: 10, height:50});
+const MyTheme = Object.assign({}, AmplifyTheme, { input: MyInput });
+
+
+//export default withAuthenticator(App, { includeGreetings: false });
+export default withAuthenticator(App, false, [], null, MyTheme);
--- src/screens/Profile/index.jsx
+++ src/screens/Profile/index.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { Text, SafeAreaView } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Auth } from "aws-amplify";
 
-const Profile = () => {
-    return <Text>Profile</Text>
+async function signOut() {
+    try {
+        console.log("Signout")
+        await Auth.signOut()
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
 }
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <Text>Profile</Text>
+      <TouchableOpacity
+        onPress={signOut}
+        style={{
+          width: "100%",
+          height: 50,
+          backgroundColor: "pink",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ fontSize: 25 }}>Signout</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
 
-export default Profile
+export default Profile;
