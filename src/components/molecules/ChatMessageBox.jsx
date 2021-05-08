9070c281d85f8fb60b5f0b0ef57e7becb3d56772
yu <hsinyu.lo@mail.utoronto.ca>
2021-05-07 23:50:12 -0700

    Add max width to chat message box

--- src/components/molecules/ChatMessageBox.jsx
+++ src/components/molecules/ChatMessageBox.jsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 import { LearnMoreLinks } from "react-native/Libraries/NewAppScreen";
 
 const ChatMessageBox = (props) => {
-  const { message, alignment } = props;
+  const { message } = props;
 
   return (
     <View style={[styles.messageBox, {alignSelf: 'flex-start'}]}>
@@ -14,6 +14,7 @@ const ChatMessageBox = (props) => {
 
 const styles = {
     messageBox: {
+        maxWidth: '70%',
         backgroundColor: '#011627',
         margin: 5,
         padding: 10,
